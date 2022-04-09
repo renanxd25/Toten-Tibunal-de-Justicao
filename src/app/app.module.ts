a7628a264157e2fec3c533a8f1eb9798182ca3ab
Renan Mendes <renanxd7x@hotmail.com>
2022-04-09 19:56:19 -0300

    primeiro codigo para o projeto do toten

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { HomeComponent } from './paginas/home/home.component';
+import { BodyHomeComponent } from './components/body-home/body-home.component';
+import { HeaderHomeComponent } from './components/header-home/header-home.component';
+import { FooterHomeComponent } from './components/footer-home/footer-home.component';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    HomeComponent,
+    BodyHomeComponent,
+    HeaderHomeComponent,
+    FooterHomeComponent,
+   
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/body-home/body-home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-body-home',
+  templateUrl: './body-home.component.html',
+  styleUrls: ['./body-home.component.scss']
+})
+export class BodyHomeComponent implements OnInit {
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+ 
+
+}
